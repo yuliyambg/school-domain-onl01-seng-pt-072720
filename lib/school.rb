71bbf1964a5ced2d23c5f050767c1089146d3c2e
secure-revert-8990 <yuliyambg@gmail.com>
2020-08-08 10:39:58 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -26,7 +26,7 @@ class School
   def sort
     sorted_hash = {}
       @roster.each do |name, grade|
-      hash.sort_by {|grade, name| [-v, k]}
+      sorted_hash.sort_by {|grade, name| [-name, grade]}
     end
   end
     
