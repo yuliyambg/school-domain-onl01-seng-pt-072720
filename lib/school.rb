0733cee64a0980e91628cadcb715b7ca20df692e
secure-revert-8990 <yuliyambg@gmail.com>
2020-08-08 10:11:42 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
 # code here!
+
+class School
+  
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def roster
+    @roster
+  end
+
+  def add_student(name,grade)
+    if @roster[grade] == nil
+      @roster[grade] = []
+    end
+    @roster[grade] << name
+  end
+
+  def grade(grade)
+    return @roster[grade]
+  end
+  
+  def sort
+  end
+    
+end
