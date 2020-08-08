0eb227ce52f3a059f5fc6343e35db61f5d480771
secure-revert-8990 <yuliyambg@gmail.com>
2020-08-08 10:57:24 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,35 @@
 # code here!
+
+class School
+  
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def roster
+    @roster
+  end
+
+  def add_student(name,grade)
+    if @roster[grade] == nil
+      @roster[grade] = []
+    end
+    @roster[grade] << name
+  end
+
+  def grade(grade)
+    return @roster[grade]
+  end
+  
+ 
+  def sort 
+    sorted = {} 
+    roster.each do |grade, name|
+    sorted[grade] = name.sort
+    end
+    sorted
+  end
+
+end
